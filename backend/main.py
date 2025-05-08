e4d730ec7da276df8cc156fa0dd5e4c1034e8336
yu1234552026 <yunn6520@gmail.com>
2025-05-09 01:59:20 +0800

    fix main.py

--- backend/main.py
+++ backend/main.py
@@ -295,4 +295,5 @@ async def api_root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=settings.DEBUG) 
+    port = int(os.environ.get("PORT", 5000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=settings.DEBUG) 
